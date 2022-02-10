c868cfcaef4b7507db985158a40b73b9c32b82e7
Anna <annabarellaalonso@gmial.com>
2022-02-10 23:10:33 +0100

    New cards added css and javascript for product view

--- javascript/productlist.js
+++ javascript/productlist.js
@@ -1,1 +1,65 @@
+console.log("Hello");
+
 const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    handleProductList(data);
+  });
+
+function handleProductList(data) {
+  data.forEach(function (item) {
+    showProduct(item);
+  });
+}
+
+/* <article class="card onSale soldOut">
+<figure class="imgwtext">
+  <img
+    class="list-img"
+    src="https://kea-alt-del.dk/t7/images/webp/640/1525.webp"
+    alt="Deck Navy Blue Backpack"
+  />
+  <figcaption class="soulOutText">Sold Out</figcaption>
+  <figcaption class="disccountNum">50%</figcaption>
+</figure>
+<h3>Deck Navy Blue Backpack</h3>
+
+<div class="price">
+  <p class="original">DKK 1900,-</p>
+  <p class="discountprice">DKK 1400,-</p>
+</div>
+<a href="../html/product.html">Read more</a>
+</article> */
+
+function showProduct(product) {
+  console.log(product);
+  const template = document.querySelector("#producttemplate").content;
+  const copy = template.cloneNode(true);
+
+  copy.querySelector(
+    ".subtle"
+  ).textContent = `${product.articletype} - ${product.brandname}`;
+
+  copy.querySelector("h3").textContent = product.productdisplayname;
+
+  copy.querySelector(".original").textContent = "DKK" + product.price;
+
+  copy.querySelector(".discountprice").textContent =
+    "DKK" +
+    Math.floor(product.price - product.price * (product.discount / 100));
+
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+
+  if (prompt.discount) {
+    copy.querySelector("article").classList.add("onSale");
+  }
+
+  const parent = document.querySelector("main");
+  parent.appendChild(copy);
+}
